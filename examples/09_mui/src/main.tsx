8d0c8a2082b4a1e936ecdbd6fdeeeaa1e3b14198
Piotr Tarasiuk <17828599+piotar@users.noreply.github.com>
2023-04-23 09:10:22 +0200

    perf: Modify packages in MUI example

--- examples/09_mui/src/main.tsx
+++ examples/09_mui/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
