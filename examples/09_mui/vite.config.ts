37c823869772dc86df8756937f1fd53ac97d94aa
Piotr Tarasiuk <17828599+piotar@users.noreply.github.com>
2023-04-26 21:11:48 +0200

    chore: Update vite config in mui example

--- examples/09_mui/vite.config.ts
+++ examples/09_mui/vite.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+    optimizeDeps: {
+        exclude: ['@mui/material', '@emotion/react', '@emotion/styled'],
+    },
+});
