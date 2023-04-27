3836ee13eae84db3e8b4af4a31e0e5eb1353c869
Piotr Tarasiuk <17828599+piotar@users.noreply.github.com>
2023-04-27 19:10:20 +0200

    chore: Fixed problem with MUI example

--- examples/09_mui/vite.config.ts
+++ examples/09_mui/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
+
+export function sourcemapExclude(): Plugin {
+    const packages = ['@mui/material', '@emotion/styled', '@emotion/react'];
+
+    return {
+        name: 'exclude-sourcemap',
+        transform(code: string, id: string) {
+            if (packages.some((pkg) => id.includes(pkg))) {
+                return {
+                    code,
+                    map: { mappings: '' },
+                };
+            }
+        },
+    };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        exclude: ['@mui/material', '@emotion/react', '@emotion/styled'],
-    },
+    plugins: [sourcemapExclude()],
 });
