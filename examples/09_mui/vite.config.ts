5b719cf16590d5ed88c6cc83df5340554b47ed98
Piotr Tarasiuk <17828599+piotar@users.noreply.github.com>
2023-04-27 20:07:18 +0200

    chore: Downgrade vitejs in MUI example

--- examples/09_mui/vite.config.ts
+++ examples/09_mui/vite.config.ts
@@ -1,22 +1,6 @@
-import { defineConfig, type Plugin } from 'vite';
-
-export function sourcemapExclude(): Plugin {
-    const packages = ['@mui/material', '@emotion/styled', '@emotion/react'];
-
-    return {
-        name: 'exclude-sourcemap',
-        transform(code: string, id: string) {
-            if (packages.some((pkg) => id.includes(pkg))) {
-                return {
-                    code,
-                    map: { mappings: '' },
-                };
-            }
-        },
-    };
-}
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [sourcemapExclude()],
+    plugins: [],
 });
