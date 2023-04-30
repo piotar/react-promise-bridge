32822eac26061c7c6dd1ff38a5bb37ce4b7ea519
Piotr Tarasiuk <17828599+piotar@users.noreply.github.com>
2023-04-30 15:03:32 +0200

    fix: Fixed useDisposePromiseBridge to handle singlas changed

--- src/hooks/useDisposePromiseBridge.test.ts
+++ src/hooks/useDisposePromiseBridge.test.ts
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { renderHook } from '@testing-library/react';
 import { useDisposePromiseBridge } from './useDisposePromiseBridge';
+import { TriggerComponentDestroyedException } from '../exceptions/TriggerComponentDestroyedException';
+import { ExternalAbortSignalException } from '../exceptions/ExternalAbortSignalException';
 
 describe('useDisposePromiseBridge', () => {
     it('should return AbortController instance', () => {
@@ -10,6 +12,7 @@ describe('useDisposePromiseBridge', () => {
         expect(result.current).instanceOf(AbortController);
         rerender();
         expect(result.current).toBe(controller);
+        expect(result.current.signal.reason).toBeUndefined();
     });
 
     it('should return AbortController instance using strict mode', () => {
@@ -20,5 +23,86 @@ describe('useDisposePromiseBridge', () => {
         expect(result.current).instanceOf(AbortController);
         rerender();
         expect(result.current).toBe(controller);
+        expect(result.current.signal.reason).toBeUndefined();
+    });
+
+    it('should return AbortController instance and abort singal after unmount', () => {
+        const { result, rerender, unmount } = renderHook(useDisposePromiseBridge, {
+            wrapper: StrictMode,
+        });
+        const controller = result.current;
+        expect(result.current.signal.reason).toBeUndefined();
+        rerender();
+        expect(result.current).toBe(controller);
+        unmount();
+        expect(result.current).toBe(controller);
+        expect(result.current.signal.reason).instanceOf(TriggerComponentDestroyedException);
+    });
+
+    it('should return AbortController instance with Aborted signal by external signals', () => {
+        const signals = [AbortSignal.abort()];
+        const { result, rerender } = renderHook(useDisposePromiseBridge, {
+            wrapper: StrictMode,
+            initialProps: signals,
+        });
+        const controller = result.current;
+        expect(result.current.signal.reason).instanceOf(ExternalAbortSignalException);
+        rerender(signals);
+        expect(result.current).toBe(controller);
+        expect(result.current.signal.reason).instanceOf(ExternalAbortSignalException);
+    });
+
+    it('should return AbortController instance with new instance after change signals', () => {
+        const signals = [AbortSignal.abort()];
+        const { result, rerender } = renderHook(useDisposePromiseBridge, {
+            wrapper: StrictMode,
+            initialProps: signals,
+        });
+        const controller = result.current;
+        expect(result.current.signal.reason).instanceOf(ExternalAbortSignalException);
+        rerender([]);
+        expect(result.current).not.toBe(controller);
+        expect(result.current.signal.reason).toBeUndefined();
+    });
+
+    it('should return AbortController instance using abort external signal', () => {
+        const externalController = new AbortController();
+        const signals = [externalController.signal];
+        const { result, rerender, unmount } = renderHook(useDisposePromiseBridge, {
+            wrapper: StrictMode,
+            initialProps: signals,
+        });
+        const controller = result.current;
+        expect(result.current).instanceOf(AbortController);
+        rerender(signals);
+        expect(result.current).toBe(controller);
+        externalController.abort();
+        expect(result.current.signal.reason).instanceOf(ExternalAbortSignalException);
+        unmount();
+        expect(result.current.signal.reason).instanceOf(ExternalAbortSignalException);
+    });
+
+    it('should call abort handler from instance AbortController hook', () => {
+        const { result, rerender, unmount } = renderHook(useDisposePromiseBridge, {
+            wrapper: StrictMode,
+        });
+        const handleAbort = vi.fn();
+        const controller = result.current;
+        controller.signal.addEventListener('abort', handleAbort);
+        expect(result.current).instanceOf(AbortController);
+        expect(result.current).toBe(controller);
+
+        const customError = new Error();
+        controller.abort(customError);
+        expect(handleAbort).toHaveBeenCalledOnce();
+        expect(result.current.signal.reason).toBe(customError);
+
+        rerender();
+        expect(result.current).not.toBe(controller);
+        expect(result.current.signal.reason).toBeUndefined();
+
+        rerender();
+        unmount();
+        expect(result.current.signal.reason).instanceOf(TriggerComponentDestroyedException);
     });
 });
--- src/hooks/useDisposePromiseBridge.ts
+++ src/hooks/useDisposePromiseBridge.ts
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { TriggerComponentDestroyedException } from '../exceptions/TriggerComponentDestroyedException';
 import { ComposeAbortController } from '../utils/ComposeAbortController';
 
 export function useDisposePromiseBridge(signals?: AbortSignal[]): AbortController {
-    const [controller, setController] = useState<AbortController>(() => new ComposeAbortController(signals));
+    const createController = useCallback(() => new ComposeAbortController(signals), [signals]);
+    const [controller, setController] = useState<AbortController>(createController);
+    const createRef = useRef(createController);
+    createRef.current = createController;
+
+    useEffect(() => {
+        const handleAbort = () => setController(createRef.current());
+        controller.signal.addEventListener('abort', handleAbort, { once: true });
+        return () => controller.signal.removeEventListener('abort', handleAbort);
+    }, [controller, createRef]);
+
     useEffect(
         () => () =>
             setController((composeController) => {
                 composeController.abort(new TriggerComponentDestroyedException());
-                return new ComposeAbortController(signals);
+                return createRef.current();
             }),
-        [],
+        [createRef.current],
     );
+
     return controller;
 }
